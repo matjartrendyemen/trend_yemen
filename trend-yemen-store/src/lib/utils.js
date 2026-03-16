d47452a17894888ab27b37ae8426a55842d72cd0
matjartrendyemen <matjartrendyemen@gmail.com>
2026-03-16 21:50:03 +0000

    astro store

--- trend-yemen-store/src/lib/utils.js
+++ trend-yemen-store/src/lib/utils.js
@@ -1,8 +1,5 @@
 export function getDirectDriveLink(url) {
   if (!url ||!url.includes('drive.google.com')) return url;
   const match = url.match(/(?:id=|\[|\/d\/|\/file\/d\/)([\w-]{25,})/);
-  if (match) {
-    return `https://drive.google.com/uc?export=view&id=${match[1]}`;
-  }
-  return url;
+  return match? `https://drive.google.com/uc?export=view&id=${match[1]}` : url;
 }
